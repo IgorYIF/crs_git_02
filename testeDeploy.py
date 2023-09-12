85c06e5951b76fe13af1796b929dbae53f5db2e7
Igor Yepes <igor@MacBook-Pro-de-Igor.local>
2023-09-12 10:56:27 -0300

    teste de atualização

--- testeDeploy.py
+++ testeDeploy.py
@@ -6,5 +6,6 @@ st.title("Teste de Streamlit")
 st. info("Digite o valor desejado")
 val1 = st.number_input("digite o valor da nota 1", 0.0, 10.0)
 st.text(vet)
+vet
 df = pd.DataFrame({'nota 1':[val1], 'nota 2':[val1], 'média':[(val1+val1)/2]})
 df
